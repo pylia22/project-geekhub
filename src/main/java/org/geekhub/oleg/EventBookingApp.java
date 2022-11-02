202d05213bcc38125404e193507c80e2df961f02
Oleg Pyliavskyi <pyliavskyi22@gmail.com>
2022-11-02 11:37:15 +0200

    refactoring code

--- src/main/java/org/geekhub/oleg/EventBookingApp.java
+++ src/main/java/org/geekhub/oleg/EventBookingApp.java
@@ -2,12 +2,11 @@ package org.geekhub.oleg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.support.JdbcUtils;
 
 @SpringBootApplication
 public class EventBookingApp {
-
     public static void main(String[] args) {
         SpringApplication.run(EventBookingApp.class, args);
     }
-
 }
